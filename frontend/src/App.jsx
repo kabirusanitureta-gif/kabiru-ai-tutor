740352286477044b9d0156afbc9829840ff63fb4
Kabiru Sani <EMAIL_DINKA>
2026-08-09 18:51:25 +0100

    Fix duplicate Layout import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "./components/Layout.jsx";
-import Layout from "./components/Layout.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 import Landing from "./pages/Landing.jsx";
